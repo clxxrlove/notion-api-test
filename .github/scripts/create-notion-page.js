afed04679707bf6711357b024e2af8cf2e8cd498
Jiyong Jung <hickerd@sch.ac.kr>
2025-12-17 20:39:25 +0900

    Update create-notion-page.js

--- .github/scripts/create-notion-page.js
+++ .github/scripts/create-notion-page.js
@@ -9,14 +9,11 @@ async function main() {
   const url = process.env.ISSUE_URL;
   const number = Number(process.env.ISSUE_NUMBER);
   const repo = process.env.REPO;
-
+  
   await notion.pages.create({
     parent: { database_id: dbId },
     properties: {
-      Name: { title: [{ text: { content: `#${number} ${title}` } }] },
-      Repo: { rich_text: [{ text: { content: repo } }] },
-      URL: { url },
-      IssueNo: { number },
+      "이름": { title: [{ text: { content: `#${number} ${title}` } }] },
     },
   });
 }
